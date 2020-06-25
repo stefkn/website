9d0a64d954aa607dc8ee8570467c0ce87d565acf
steken <stekenno@gmail.com>
2020-06-25 22:00:30 +0100

    Update styling on non index pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Layout from '@common/Layout';
 import { Container } from '@components/global';
 
+import Footer from '@sections/Footer';
+import Navbar from '@common/Navbar';
+
+import styled from 'styled-components';
+
 const NotFoundPage = () => (
   <Layout>
-    <Container>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    </Container>
+    <Navbar />
+    <MainMatter>
+      <h1>404 Not Found</h1>
+      <p>Sad face.</p>
+      <Link to="/">Go back to the homepage</Link>
+    </MainMatter>
+    <Footer />
   </Layout>
 );
+
+const MainMatter = styled.div`
+  margin-top: 26vh;
+  margin-bottom: 12vh;
+  padding-left: 17vw;
+  padding-right: 17vw;
+
+  h2 {
+    margin-bottom: 16px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    text-align: left;
+    // margin-bottom: 96px;
+  }
+`;
 
 export default NotFoundPage;
--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -4,14 +4,38 @@ import { Link } from 'gatsby';
 import Layout from '@common/Layout';
 import { Container } from '@components/global';
 
+import Footer from '@sections/Footer';
+import Navbar from '@common/Navbar';
+
+import styled from 'styled-components';
+
 const AboutMe = () => (
   <Layout>
-    <Container>
-      <h1>Hi from the second page</h1>
-      <p>Welcome to page 2</p>
+    <Navbar />
+    <MainMatter>
+      <h1>About Me</h1>
+      <p>Hello.</p>
       <Link to="/">Go back to the homepage</Link>
-    </Container>
+    </MainMatter>
+    <Footer />
   </Layout>
 );
+
+const MainMatter = styled.div`
+  margin-top: 26vh;
+  margin-bottom: 12vh;
+  padding-left: 17vw;
+  padding-right: 17vw;
+
+  h2 {
+    margin-bottom: 16px;
+  }
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    grid-template-columns: 1fr;
+    text-align: left;
+    // margin-bottom: 96px;
+  }
+`;
 
 export default AboutMe;

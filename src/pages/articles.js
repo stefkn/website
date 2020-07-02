c06b5b6ad0728d507b6bad252ceca87c33c7837a
Stefan Nowak <stefan.nowak@tails.com>
2020-07-02 11:09:44 +0100

    add basic articles index

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -8,18 +8,27 @@ import Footer from '@sections/Footer';
 import Navbar from '@common/Navbar';
 
 import styled from 'styled-components';
+import PostLink from "../components/common/PostLink"
 
-const Articles = () => (
-  <Layout>
+const Articles = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+  return <Layout>
     <Navbar />
     <MainMatter>
       <h1>Articles</h1>
       <p>Here are some articles:</p>
+      <div>{Posts}</div>
       <Link to="/">Go back to the homepage</Link>
     </MainMatter>
     <Footer />
   </Layout>
-);
+}
 
 const MainMatter = styled.div`
   margin-top: 26vh;
@@ -39,3 +48,21 @@ const MainMatter = styled.div`
 `;
 
 export default Articles;
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+          }
+        }
+      }
+    }
+  }
+`

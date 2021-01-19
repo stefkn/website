ec6a2e22dccf760cd8e6d032e7a5616bf3694a10
Stefan Nowak <stefan.nowak@tails.com>
2021-01-19 23:18:55 +0000

    import and use HeaderAnimation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Layout from '@common/Layout';
 import Navbar from '@common/Navbar';
 
+import HeaderAnimation from '@components/HeaderAnimation'
 import Header from '@sections/Header';
 import About from '@sections/About';
 import Brands from '@sections/Brands';
@@ -12,6 +13,7 @@ import Footer from '@sections/Footer';
 
 const IndexPage = () => (
   <Layout>
+    <HeaderAnimation />
     <Navbar />
     <Header />
     <About />

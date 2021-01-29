13886e6297e63176102be51de583d689a05d15a8
Stefan Nowak <stefan.nowak@tails.com>
2021-01-29 11:01:29 +0000

    remove imports

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,6 @@ import Navbar from '@common/Navbar';
 import HeaderAnimation from '@components/HeaderAnimation'
 import Header from '@sections/Header';
 import About from '@sections/About';
-import Brands from '@sections/Brands';
-import Team from '@sections/Team';
-import Faq from '@sections/Faq';
 import Footer from '@sections/Footer';
 
 const IndexPage = () => (
@@ -17,9 +14,6 @@ const IndexPage = () => (
     <Navbar />
     <Header />
     <About />
-    {/* <Brands /> */}
-    {/* <Team /> */}
-    {/* <Faq /> */}
     <Footer />
   </Layout>
 );

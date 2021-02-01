14f848c33fb4b016a0f53e844412ccab795d6e54
Stefan Nowak <stefan.nowak@tails.com>
2021-02-01 19:32:14 +0000

    hacky fix for flashy bg loading and graceful JS crash handling

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,20 @@ import Header from '@sections/Header';
 import About from '@sections/About';
 import Footer from '@sections/Footer';
 
+import styled from 'styled-components';
+
+const HeaderAnimationBackground = styled.div`
+position: absolute;
+max-width: 100vw;
+width: 100%;
+height: 55em;
+display: block;
+background: #2f3be7;
+    `
+
 const IndexPage = () => (
   <Layout>
+    <HeaderAnimationBackground />
     <HeaderAnimation />
     <Navbar />
     <Header />
